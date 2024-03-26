86f71da7b9a97a440708c3144af5f7ef99bf2bf6
Theophelus <theotheo20001@gmail.com>
2024-03-26 15:19:13 +0700

    added adaptivity and the js is now working

--- index.js
+++ index.js
@@ -1,89 +1,23 @@
-const questionBtns = document.querySelectorAll('.question-btn');
-questionBtns.forEach(btn => {
-    btn.addEventListener('click', function(){
-        this.classList.toggle('active');
-        const answers = this.nextElementsSibling;
-        const plusIcon = this.querySelector('.plus-icon');
-        const minusIcon = this.querySelector('.minus-icon');
+var acc = document.getElementsByClassName("accordion-btn");
+var i;
 
-        if(answers.style.maxHeight){
-            answers.style.maxHeight = null;
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        var plusIcon = this.querySelector('.plus-icon');
+        var minusIcon = this.querySelector('.minus-icon');
+
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
             plusIcon.style.display = 'block';
             minusIcon.style.display = 'none';
         } else {
-            answers.style.maxHeight = answers.scrollHeight + "px";
-            plusIcon.style.display = 'block';
-            minusIcon.style.display = 'none';
+            panel.style.maxHeight = panel.scrollHeight + "px" ;
+            plusIcon.style.display = 'none';
+            minusIcon.style.display = 'block';
         }
     })
-})
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// document.getElementsByClassName('btn').click(function(){
-//     document.getElementsByClassName('btn').("purple")
-// })  
-
-//  HTML
-
-
-// JavaScript
-// var changeColor = document.querySelector("#changeColor");
-
-// changeColor.addEventListener("click", function() {
-//   changeColor.style.color = "red";
-// });
-// function myFunc() {
-//     let para = document. querySelector(".p");
-//     para.classList.toggle("p");
-
-// }
-// document.querySelector('#try').addEventListener("click", (Event)=> {
-//     Event.target.classList.toggle("red");
-// });
-
+    
